e291631b4d8573993bfa84fdd79b683660655ede
cunxinshuihua <cunxinshuihua@foxmail.com>
2015-05-02 19:58:39 +0800

    Update single_word.cpp

--- single_word.cpp
+++ single_word.cpp
@@ -1,2 +1,14 @@
 #include "single_word.h"
+#include "count.cpp"
 #include <iostream>
+#include <ctime>
+
+int current_time()
+{
+	time_t t = time(0);   // get time now
+    struct tm * now = localtime( & t );
+    //now-> tm_year return this year's number delete 1900;
+    //now-> tm_mou return mouth;
+    //now-> tm_mday return day's number;
+    //we can caculate the day through to come true the time count;
+}
