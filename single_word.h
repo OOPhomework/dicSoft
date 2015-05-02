c4f9d276d3dbe785130102ccf7674e2c5dd3d6a9
cunxinshuihua <cunxinshuihua@foxmail.com>
2015-05-02 19:58:11 +0800

    Update single_word.h

--- single_word.h
+++ single_word.h
@@ -1,2 +1,33 @@
+#ifndef SINGLE_WORD_H
+#define SINGLE_WORD_H
+
 #include "mnemonic.h"
 #include "file_operator.h"
+#include <iostream>
+enum LABLE{
+	familiar_word,
+	new_word,
+	unstable_word
+}
+class single_word
+{
+public:
+	single_word();
+	int current_time();//return current time;
+	void get_relative_map();
+	void set_lable();
+	friend ostream& operator<< (ostream &os,const single_word T&);
+	~single_word();
+private:
+	string phonetic_symbol;//yinbiao
+	string part_of_speech;//cixing
+	string paraphrase;//shiyi
+	string eg;
+	LABLE lable;
+	int current_time;
+
+};
+
+ostream& operator<< (ostream &os,const single_word T&);
+
+#endif
